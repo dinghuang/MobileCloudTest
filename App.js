87fbb8561137da2f77240188f5dd8b4d07e3a3ff
dinghuang <huang.ding@hand-china.com>
2017-12-14 09:54:17 +0800

    首页

--- App.js
+++ App.js
@@ -1,57 +1,71 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react';
+import { StatusBar,StyleSheet, Text, View, Button, Alert,TouchableHighlight } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
 
-import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: '首页', 
+    headerTitleStyle :{
+      fontFamily: 'PingFangSC-Medium',
+      textAlign:'center',
+      alignSelf:'center',
+      color:'#FFFFFF',
+      opacity:0.87,
+      fontSize:18
+    },
+    headerStyle:{
+    backgroundColor:'#3F51B5',
+    },
+    headerLeft: (
+      <Icon
+        name= "md-menu"
+        onPress={() => Alert.alert(
+          'Alert Title',
+          '打开侧边栏',
+        )}
+        size={20}
+        color='#FFFFFF'
+        style={{marginLeft :12, color:'#FFFFFF'}}
+      />
+    ),
+    headerRight: (
+      <Icon 
+        backgroundColor="#3F51B5" 
+      />
+    )
+  };
+  
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <StatusBar
+          backgroundColor="#3F51B5"
+          barStyle="light-content"
+        />
+        <Text>欢迎来到首页</Text>
+      </View>
+    )
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
 });
 
-export default class App extends Component<{}> {
+export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
+    return  <SimpleApp />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#fff',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    justifyContent: 'center'
+  }
 });
